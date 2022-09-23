7296e2d579fde10820e6c9fa4a1eaf2b8077a1fd
Jagpat Singh <jsingh14192@gmail.com>
2022-09-23 20:50:50 +0530

    moved request handlers from routes to controllers

--- api/controllers/orders.js
+++ api/controllers/orders.js
@@ -1,6 +1,6 @@
 const Order = require('../models/order');
 
-const getAllOrders = (req, res, next) => {
+exports.getAllOrders = (req, res, next) => {
     
     Order.find()
         .populate('product', 'name')
@@ -27,5 +27,52 @@ const getAllOrders = (req, res, next) => {
         })
 };
 
+exports.createOrder = (req, res, next) => {
+    const order = new Order({
+        _id: mongoose.Types.ObjectId(),
+        product: req.body.productId,
+        quantity: req.body.quantity
+    });
+    order.save()
+        .then((data) => {
+            console.log(data);
+            res.status(201).json({
+                message: "created a new order",
+                createdOrder: data,
+                request: {
+                    type: 'GET',
+                    url: 'http://localhost:3000/orders/' + data._id
+                }
+            });
+        })
+        .catch((err) => {
+            res.status(500).json(err);
+        })
+}
 
-module.exports = getAllOrders;
+exports.getOrderById = (req, res, next) => {
+    const id = req.params.orderId;
+    Order.findById(id)
+        .populate('product')
+        .then((data) => {
+            
+            if(!data){
+                res.status(404).json({message: "Order not found"});
+            } else {
+                console.log(data);
+                res.status(200).json(data)
+            }
+        })
+        .catch((err) => {
+            res.status(500).json(err);
+        })
+}
+
+exports.deleteOrder = (req, res, next) => {
+    res.status(201).json({
+        message: 'Order deleted',
+        Id: req.params.orderId
+    });
+}
+
+// module.exports = getAllOrders;
--- api/routes/orders.js
+++ api/routes/orders.js
@@ -3,56 +3,14 @@ const { default: mongoose } = require('mongoose');
 const router = express.Router();
 const checkAuth = require('../middleware/check-auth');
 const Order = require('../models/order');
-const getAllOrders = require('../controllers/orders');
+const orderController = require('../controllers/orders');
 
-router.get('/', checkAuth, getAllOrders);
+router.get('/', checkAuth, orderController.getAllOrders);
 
-router.post('/', checkAuth, (req, res, next) => {
-    const order = new Order({
-        _id: mongoose.Types.ObjectId(),
-        product: req.body.productId,
-        quantity: req.body.quantity
-    });
-    order.save()
-        .then((data) => {
-            console.log(data);
-            res.status(201).json({
-                message: "created a new order",
-                createdOrder: data,
-                request: {
-                    type: 'GET',
-                    url: 'http://localhost:3000/orders/' + data._id
-                }
-            });
-        })
-        .catch((err) => {
-            res.status(500).json(err);
-        })
-});
+router.post('/', checkAuth, orderController.createOrder);
 
-router.get('/:orderId', checkAuth, (req, res, next) => {
-    const id = req.params.orderId;
-    Order.findById(id)
-        .populate('product')
-        .then((data) => {
-            
-            if(!data){
-                res.status(404).json({message: "Order not found"});
-            } else {
-                console.log(data);
-                res.status(200).json(data)
-            }
-        })
-        .catch((err) => {
-            res.status(500).json(err);
-        })
-});
+router.get('/:orderId', checkAuth, orderController.getOrderById);
 
-router.delete('/:orderId', checkAuth, (req, res, next) => {
-    res.status(201).json({
-        message: 'Order deleted',
-        Id: req.params.orderId
-    });
-});
+router.delete('/:orderId', checkAuth, orderController.deleteOrder);
 
 module.exports = router;
